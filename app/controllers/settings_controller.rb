1ef218b1a19544686692c1859839ac04977fb554
Jason Mannon <jmannonpdx@gmail.com>
2015-10-13 17:19:37 -0400

    Adds web url to settings varaible

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -8,7 +8,7 @@ class SettingsController < ApplicationController
   def create
     settings = Setting.create(settings_params)
     if settings.save
-      redirect_to root_url
+      redirect_to settings_path, notice: "Success"
     else
       redirect_to settings_path, notice: "There was an error"
     end
@@ -17,7 +17,7 @@ class SettingsController < ApplicationController
   def update
     settings = Setting.find(params[:id])
     if settings.update(settings_params)
-      redirect_to root_url, notice: "Success"
+      redirect_to settings_path, notice: "Success"
     else
       redirect_to settings_path, notice: "There was an error"
     end
@@ -26,7 +26,7 @@ class SettingsController < ApplicationController
   private
 
   def settings_params
-    params.require(:setting).permit(:channel_type, :name, :bot_id, :bot_name)
+    params.require(:setting).permit(:channel_type, :name, :bot_id, :bot_name, :web_url)
   end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151012194014) do
+ActiveRecord::Schema.define(version: 20151013204754) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(version: 20151012194014) do
     t.string "name",         default: "Standup"
     t.string "bot_id"
     t.string "bot_name"
+    t.string "web_url"
   end
 
   create_table "standups", force: :cascade do |t|
