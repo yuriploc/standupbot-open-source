5fc12b2c69833219bedcad20307a4660f9b043d1
Jason Mannon <jmannonpdx@gmail.com>
2015-10-13 20:32:54 -0400

    Sort standups by complete

--- app/controllers/standups_controller.rb
+++ app/controllers/standups_controller.rb
@@ -3,7 +3,7 @@ class StandupsController < ApplicationController
   def index
     @date = Date.parse(params[:date]) rescue Date.today
     @date_string = @date.strftime("%A")
-    @standups = Standup.where(created_at: @date.at_midnight..@date.next_day.at_midnight)
+    @standups = Standup.where(created_at: @date.at_midnight..@date.next_day.at_midnight).order('today DESC NULLS LAST') 
   end
 
 end
