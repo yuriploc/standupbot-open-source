a12f545c15c40307566b46bb6cca4ac7131a19d4
Jason Mannon <jmannonpdx@gmail.com>
2015-10-19 17:32:41 -0400

    sort standups randomly

--- app/controllers/standups_controller.rb
+++ app/controllers/standups_controller.rb
@@ -3,7 +3,7 @@ class StandupsController < ApplicationController
   def index
     @date = Date.parse(params[:date]) rescue Date.today
     @date_string = @date.strftime("%A")
-    @standups = Standup.where(created_at: @date.at_midnight..@date.next_day.at_midnight).order('today DESC NULLS LAST') 
+    @standups = Standup.where(created_at: @date.at_midnight..@date.next_day.at_midnight).sort_by { rand }
   end
 
 end
